d8c8d4e5ba5dd7f8e1c472f17b91a83f29843ebb
IanMezza <ian.mezza@gmail.com>
2017-06-21 23:43:51 -0500

    External WP variables for modulatity

---
+++ wp_environment.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Wordpress global variables.
+
+These variables are intended to keep environmental variables such as paths
+and public directories out of the public information.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+wordpress_installation = {'sitename':'sitename', 'production_path':'path_to_wordpress_root_directory', 'backup_path':'path_to_backup_directory'}
